f3e8dd0699b7dc3e94655ae427e6ee0d1182db8d
onting <hemool123@naver.com>
2019-12-05 21:19:29 +0900

    Add terminal file

---
+++ terminal.c
@@ -0,0 +1,10 @@
+/*
+Commands:
+ls: List all pending requests.
+ls-rule: List all rules.
+add-rule: Add rule
+del-rule Delete rule
+ap: Approve request(one or more)
+di: Deny request(one or more)
+exit, quit: Terminate program 
+*/
